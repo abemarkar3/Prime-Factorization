93aa9eee7dbfdc606e9e0b162650bf502b3c15da
Abemarkar23 <abemarkar23@gmail.com>
2017-10-01 17:39:42 -0700

    added while loop and exeption handling

--- PrimeFactorization.py
+++ PrimeFactorization.py
@@ -3,32 +3,36 @@ import os
 Prime Factorization - Have the user enter a number and find all Prime Factors (if there are any) and display them.
 '''
 
+while True:
 
-checker = 0
-num = input('enter number of which you want prime factors: ')
-origNum = num
-primeFactors = []
+    try:
 
-for x in [2,3,5,7]:
-    if num%x == 0:
-        pass
-    else:
-        checker += 1
-    if checker == 4:
-        print '{} is Prime'.format(num)
-        primeFactors.append(num)
-        primeFactors.append(1)
+        checker = 0
+        num = input('enter number of which you want prime factors: ')
+        origNum = num
+        primeFactors = []
 
-def makeFactors(which):
-    global num
+        for x in [2,3,5,7]:
+            if num%x == 0:
+                pass
+            else:
+                checker += 1
+            if checker == 4:
+                print '{} is Prime'.format(num)
+                primeFactors.append(num)
+                primeFactors.append(1)
 
-    if num%which == 0:
-        num = num/which
-        primeFactors.append(which)
-    else:
-        return True
+        def makeFactors(which):
+            global num
 
-for x in [2,3,5,7]:
-    makeFactors(x)
+            while num%which == 0:
+                num = num/which
+                primeFactors.append(which)
 
-print 'Prime factors of {} are {} and 1'.format(origNum, primeFactors)
+        for x in [2,3,5,7]:
+            makeFactors(x)
+
+        print 'Prime factors of {} are {} and 1'.format(origNum, primeFactors)
+
+    except NameError:
+        print 'please enter a number'
