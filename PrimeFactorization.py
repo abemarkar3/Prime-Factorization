33cd12f615bc31f8d666119924199985fe09a3e0
Abemarkar23 <abemarkar23@gmail.com>
2017-10-01 17:27:02 -0700

    My First Commit

---
+++ PrimeFactorization.py
@@ -0,0 +1,34 @@
+import os
+'''
+Prime Factorization - Have the user enter a number and find all Prime Factors (if there are any) and display them.
+'''
+
+
+checker = 0
+num = input('enter number of which you want prime factors: ')
+origNum = num
+primeFactors = []
+
+for x in [2,3,5,7]:
+    if num%x == 0:
+        pass
+    else:
+        checker += 1
+    if checker == 4:
+        print '{} is Prime'.format(num)
+        primeFactors.append(num)
+        primeFactors.append(1)
+
+def makeFactors(which):
+    global num
+
+    if num%which == 0:
+        num = num/which
+        primeFactors.append(which)
+    else:
+        return True
+
+for x in [2,3,5,7]:
+    makeFactors(x)
+
+print 'Prime factors of {} are {} and 1'.format(origNum, primeFactors)
